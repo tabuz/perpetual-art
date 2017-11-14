6414097f20c3d888bc2515c5a3baea4c0646a3db
Hubert Zelek <zelek.hubert@gmail.com>
2017-11-14 22:55:02 +0000

    Switch hero to use flexbox, hero styling

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,7 +6,7 @@
 
   	function setHeroSize() {
     	var browserHeight = jQuery(window).height()
-    	$('.hero-wrapper').css('height', browserHeight);
+    	$('.hero-wrapper').css('height', browserHeight - 100px);
     	$('.hero').css('height', 0.75 * browserHeight);
     	$('.hero').css('width', 0.75 * browserHeight);
     	//$('.hero').css('left', '150px');
