c30ca2a6b78d58787cc2fe17f70d665c62b61b0e
Hubert Zelek <zelek.hubert@gmail.com>
2017-11-14 00:00:44 +0000

    Work on making hero responsvie

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -11,7 +11,21 @@
   'use strict';
 
   $(function () {
-    // FastShell
+
+  	function setHeroSize() {
+    	var browserHeight = jQuery(window).height()
+    	$('.hero-wrapper').css('height', browserHeight);
+    	$('.hero').css('height', 0.75 * browserHeight);
+    	$('.hero').css('width', 0.75 * browserHeight);
+    	//$('.hero').css('left', '150px');
+	}
+
+    $(window).on('resize', function () {
+    	console.log('resize');
+    	setHeroSize();
+	});
+
+	setHeroSize();
   });
 
 })(jQuery, window, document);
