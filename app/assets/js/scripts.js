0b99e6e15cca27df978bf7cb9ab5204fca359a0f
Hubert Zelek <zelek.hubert@gmail.com>
2017-11-25 22:31:36 +0000

    Template refactoring, home caption styling

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -11,26 +11,49 @@
   'use strict';
 
   $(function () {
-  	
-   	function setHeroSize() {
-    	var browserHeight = jQuery(window).height();
-    	var browserWidth = jQuery(window).width();
 
-    	$('.hero-wrapper').css('height', (browserHeight));
-    	$('.hero').css('height', (0.75 * browserWidth));
-    	$('.hero').css('width', (0.75 * browserWidth));
+    function setHeroSize() {
+      var browserHeight = jQuery(window).height();
+      var browserWidth = jQuery(window).width();
 
-    	var wrapperHeight = Number($('.hero-wrapper').css('height').replace('px', ''));
-    	var heroHeight = Number($('.hero').css('height').replace('px', ''));
+      $('.hero-wrapper').css('height', (browserHeight));
+      $('.hero').css('height', (0.75 * browserWidth));
+      $('.hero').css('width', (0.75 * browserWidth));
 
-    	$('.hero-container').css('margin-top', (wrapperHeight - heroHeight)/2);
-	}
+      var wrapperHeight = Number($('.hero-wrapper').css('height').replace('px', ''));
+      var heroHeight = Number($('.hero').css('height').replace('px', ''));
+
+      $('.hero-container').css('margin-top', (wrapperHeight - heroHeight)/2);
+    }
+
+    function setElementsWidth() {
+      console.log($('.nav-wrapper.container').css('width'), $('.home-caption'));
+      $('.wrapper').css('width', $('.nav-wrapper.container').css('width'));
+      $('.home-caption').css('width', $('.nav-wrapper ul').css('width'));
+    }
 
     $(window).on('resize', function () {
     	setHeroSize();
-	});
+      setElementsWidth();});
 
-	setHeroSize();
+      setHeroSize();
+      setElementsWidth();
   });
 
 })(jQuery, window, document);
+
+
+// Reveal on scrool
+//
+// $(window).scroll(function() {
+//     var $falseHeader = $('.hero-wrapper'),
+//         $heroMask = $('#hero-mask');
+//     var $shadower = $('.hero-wrapper');
+//     var wScroll = $(this).scrollTop();
+//     var headScroll = (-wScroll / 4 );
+//     var faderScroll = (wScroll / 900);
+//     var fadeToColor = Math.min(faderScroll, 1);
+//     $heroMask.css('filter', 'invert(' + fadeToColor + ')');
+//     $shadower.css('background-color', 'rgba(0,0,0,' + fadeToColor + ')');
+
+// });
