e2c0e93d403f9f4690449e8521c9ecacabe25dcd
Hubert Zelek <zelek.hubert@gmail.com>
2017-12-24 14:02:22 +0000

    Added materialize sass source code, start work on site footer

--- gulpfile.js
+++ gulpfile.js
@@ -69,4 +69,5 @@ gulp.task('default', ['css', 'js', 'browser-sync'], function () {
     gulp.watch("src/scss/**/*.scss", ['css']);
     gulp.watch("src/js/*.js", ['js']);
     gulp.watch("app/*.html", ['bs-reload']);
+    gulp.watch("app/assets/css/*.css", ['bs-reload']);
 });
